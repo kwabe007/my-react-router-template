baf9ae0194430922bca52833d8048c1dd5d07821
kwabe007 <kwabe007@users.noreply.github.com>
2025-02-03 11:36:16 +0100

    Add useHydrated hook

--- app/utils.ts
+++ app/utils.ts
@@ -1,5 +1,5 @@
 import { useMatches } from 'react-router'
-import { useMemo } from 'react'
+import { useMemo, useSyncExternalStore } from 'react'
 import type { User } from '~/domains/users/user-schema'
 
 const DEFAULT_REDIRECT = '/'
@@ -109,3 +109,29 @@ export function getOrThrow<T>(
   }
   return value
 }
+
+/**
+ * Return a boolean indicating if the JS has been hydrated already.
+ * When doing Server-Side Rendering, the result will always be false.
+ * When doing Client-Side Rendering, the result will always be false on the
+ * first render and true from then on. Even if a new component renders it will
+ * always start with true.
+ *
+ * Example: Disable a button that needs JS to work.
+ * ```tsx
+ * let hydrated = useHydrated();
+ * return (
+ *   <button type="button" disabled={!hydrated} onClick={doSomethingCustom}>
+ *     Click me
+ *   </button>
+ * );
+ * ```
+ */
+export function useHydrated() {
+  return useSyncExternalStore(
+    subscribe,
+    () => true,
+    () => false
+  )
+}
+
